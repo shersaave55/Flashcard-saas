8ec42b9e696ecd1c269e9a65c549b6177d637957
shersaave55 <ssaavedra27@amherst.edu>
2024-08-18 18:32:16 -0400

    update import in firebase

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import 'firebase/firestore';
-import {initializeApp} from "firebase/app";
-import {getFirestore, initializeApp} from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
